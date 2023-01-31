ec87e9ff4abc1fbe38b5143374ad7d170e2f6304
Cody Hilborn <codyhilborn11@gmail.com>
2023-01-31 22:13:18 +0000

    Add new js file for char count, implement logic to change value based on user input

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+  // >>> Change value of char counter using length of input.
+  const $tweetText = $('#tweet-text');
+  const $counter = $('.counter');
+
+  $($tweetText).on('input', function() {
+    $counter.val(140 - $(this).val().length);
+  });
+
+
+
+});
